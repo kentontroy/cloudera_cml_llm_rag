bc1f5cc787390cfab447e86ae2e92e481039b369
Kenton Troy <kentontroy@yahoo.com>
2023-10-28 21:48:53 -0400

    Add files via upload

---
+++ download_models.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -P /home/cdsw/models https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q5_K_S.gguf
+wget -P /home/cdsw/models https://huggingface.co/TheBloke/Mistral-7B-v0.1-GGUF/resolve/main/mistral-7b-v0.1.Q4_K_M.gguf
+wget -P /home/cdsw/models https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q4_K_M.gguf
