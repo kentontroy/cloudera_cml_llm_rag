2e67c9edcc0716917aadcd81902294e4062d5a24
Kenton Troy <kentontroy@yahoo.com>
2023-11-15 09:25:26 -0500

    Update install_dependencies.sh

--- jobs/install_dependencies.sh
+++ jobs/install_dependencies.sh
@@ -1,2 +1,2 @@
-!pip install --upgrade pip
-!pip install --no-cache-dir --log pip-req.log -r /home/cdsw/requirements.txt
+pip install --upgrade pip
+pip install --no-cache-dir --log pip-req.log -r /home/cdsw/requirements.txt
